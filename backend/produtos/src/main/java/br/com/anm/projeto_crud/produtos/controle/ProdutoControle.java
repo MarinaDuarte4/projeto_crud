680d0302edc5c78a92c8943f198772ee9b5b3ba0
Marina <marina.lopes4@aluno.ce.gov.br>
2025-03-10 08:47:31 -0300

    Criação da rota /listar

--- backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/controle/ProdutoControle.java
+++ backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/controle/ProdutoControle.java
@@ -1,13 +1,30 @@
 package br.com.anm.projeto_crud.produtos.controle;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.anm.projeto_crud.produtos.Servico.ProdutoServico;
+import br.com.anm.projeto_crud.produtos.modelo.ProdutoModelo;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 @RestController
 public class ProdutoControle {
+
+    @Autowired
+    private ProdutoServico ps;
+    
+    @GetMapping("/listar")
+    public Iterable<ProdutoModelo>listar(){
+        return ps.listar();
+
+    }
+
     @GetMapping("/")
     public String rota(){
         return "A API está funcionando!";
     }
+
 }
