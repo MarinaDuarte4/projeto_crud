7ebd6ffde185c2cda3dd5661b6f428aac9156d0b
Marina <marina.lopes4@aluno.ce.gov.br>
2025-03-18 11:22:31 -0300

    Alterção no método cadastrar

--- backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/Servico/ProdutoServico.java
+++ backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/Servico/ProdutoServico.java
@@ -1,10 +1,13 @@
 package br.com.anm.projeto_crud.produtos.Servico;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.anm.projeto_crud.produtos.Repositorio.ProdutoRepositorio;
 import br.com.anm.projeto_crud.produtos.modelo.ProdutoModelo;
+import br.com.anm.projeto_crud.produtos.modelo.RespostaModelo;
 
 @Service
 public class ProdutoServico {
@@ -12,8 +15,41 @@ public class ProdutoServico {
     @Autowired
     private ProdutoRepositorio pr;
 
+    private RespostaModelo rm;
+
+    //Listar produto
     public Iterable<ProdutoModelo> listar(){
         return pr.findAll();
 
     }
+
+    public ResponseEntity <?> cadastrarAlterar(ProdutoModelo pm, String acao){
+        if(pm.getNome().equals("")){
+            rm.setResposta("O nome do produto é obrigatório!");
+            return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+        }else if(pm.getMarca().equals("")){
+            rm.setResposta("A marca do produto é obrigatório!");
+            return new ResponseEntity<RespostaModelo>(rm,HttpStatus.BAD_REQUEST);
+        }else{
+            if(acao.equals("cadastrar")){
+                return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.CREATED);
+            }else{
+                return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.OK);
+                
+            }
+        }
+    }
+
+    //Cadastrar produtos
+    public ResponseEntity<?> cadastrar(ProdutoModelo pm){
+        if (pm.getNome().equals("")){
+            rm.setResposta("O nome do produto é obrigatório!");
+            return new ResponseEntity<RespostaModelo>(rm,HttpStatus.BAD_REQUEST);
+        }else if (pm.getMarca().equals("")){
+            rm.setResposta("O nome da marca do produto é obrigatório!");
+            return new ResponseEntity<RespostaModelo>(rm,HttpStatus.BAD_REQUEST);
+        }else{
+            return new ResponseEntity<ProdutoModelo>(pr.save(pm) ,HttpStatus.CREATED);
+        }
+}
 }
--- backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/controle/ProdutoControle.java
+++ backend/produtos/src/main/java/br/com/anm/projeto_crud/produtos/controle/ProdutoControle.java
@@ -6,7 +6,11 @@ import br.com.anm.projeto_crud.produtos.Servico.ProdutoServico;
 import br.com.anm.projeto_crud.produtos.modelo.ProdutoModelo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 
@@ -15,11 +19,22 @@ public class ProdutoControle {
 
     @Autowired
     private ProdutoServico ps;
-    
+ 
+
     @GetMapping("/listar")
     public Iterable<ProdutoModelo>listar(){
         return ps.listar();
 
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm){
+        return ps.cadastrarAlterar(pm, "cadastrar");
+    }
+    
+    @PutMapping("/alterar")
+    public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm){
+        return ps.cadastrarAlterar(pm, "alterar");
     }
 
     @GetMapping("/")
