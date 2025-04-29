83cb00b3e8e651f91b8509698db2c25ae937dfe9
Marina <marina.lopes4@aluno.ce.gov.br>
2025-04-29 18:55:18 -0300

    Front conectado ao Back, Dados da tabela carregados e correção

--- frontend/front/src/App.js
+++ frontend/front/src/App.js
@@ -1,16 +1,23 @@
-import { useState } from 'react';
-import './App.css';
-import Formulario from './Formulario';
-import Tabela from './Tabela';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Formulario from "./Formulario";
+import Tabela from "./Tabela";
 
 function App() {
+  const [btnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
 
-  const[btnCadastrar] =useState(false);
+  //UseEffect
+  useEffect(() => {
+    fetch("http://localhost:8080/listar")
+      .then((retorno) => retorno.json())
+      .then((retorno_convertido) => setProdutos(retorno_convertido));
+  }, []);
 
   return (
     <div>
-      <Formulario botao={btnCadastrar}/>
-      <Tabela/>
+      <Formulario botao={btnCadastrar} />
+      <Tabela vetor={produtos}/>
     </div>
   );
 }
